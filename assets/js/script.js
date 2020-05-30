9490a72fbc58cea3a3c9180af6495f592ec84d5c
MasonM <Mason.P.Marinko@gmail.com>
2020-05-30 16:05:21 -0600

    Trying to update blur to button press

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,71 +2,107 @@ var tasks= {};
 var topTimeEl = document.querySelector("#currentDay");
 var currentTime = moment();
 
+
 topTimeEl.textContent = currentTime.format("dddd MMM DD, YYYY")
 
 // due date was clicked
 $(".list-group").on('click', "ul",  function () {
     // get current text
     var userInput = $(this).text().trim();
-  
+    console.log(userInput)
     // create new input element
     var taskInput = $("<input>")
       .attr("type", "text")
       .val(userInput);
-  
+
+
     // swap out elements
     $(this).replaceWith(taskInput);
-  
-    // enable jquery ui datepicker
-    taskInput.on({
-      onClose: function () {
-        // when calendar is closed, force a "change" event on the 'taskInput'
-        $(this).trigger("change");
-      }
-    });
-  
-    // automatically bring up
-    taskInput.trigger("focus");
+    console.log(this)
   });
 
 
 
+  var auditTask = function () {
+    // get time from task element
+    // var date = $(taskEl).find("span").text().trim();
+    // convert to moment object at 5:00PM
+    var time = moment().set("hour", 9);
+    // remove any old classes from element
+    $(".date-9").removeClass("list-group-item-warning list-group-item-danger");
+    if (moment().isAfter(time)) {
+      $(taskEl).addClass("list-group-item-danger");
+    } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+      $(taskEl).addClass("list-group-item-warning");
+    }
+  };
+
+
+  
 
 
 
 
-//   // value of due userInput was changed
-// $(".list-group").on("change", "input[type='text']", function () {
-//     // get current text
-//     var userInput = $(this)
-//       .val()
+
+
+
+
+
+
+
+
+
+
+    
+    // taskInput.on({
+    //   onClose: function () {
+    //     // when calendar is closed, force a "change" event on the 'taskInput'
+    //     $(this).trigger("change");
+    //     console.log(this)
+    //   }
+    // });
   
-//     // get the parent ul's id attribute
-//     var status = $(this)
-//       .closest(".list-group")
-//       .attr("id")
-//       .replace("list-", "");
+    // automatically bring up
+    // taskInput.trigger("focus");
+
+
+
+
+
+
+  // value of due userInput was changed
+$(".list-group").on("blur", "input[type='text']", function () {
+    // get current text
+    var userInput = $(this)
+      .val()
+      console.log(userInput)
   
-//     // get the task's position in the list of other li elements
-//     var index = $(this)
-//       .closest(".listItem")
-//       .index();
+    // // get the parent ul's id attribute
+    // var status = $(this)
+    //   .closest(".list-group")
+    //   .attr("id")
+    //   .replace("list-", "");
   
-//     // update task in array and re-save to localstorage
-//     tasks[status][index].userInput = userInput;
-//     saveTasks();
+    // // get the task's position in the list of other li elements
+    // var index = $(this)
+    //   .closest(".listItem")
+    //   .index();
   
-//     // recreate span element with bootstrap classes
-//     var taskSpan = $("<li>")
-//       .addClass("badge badge-primary badge-pill")
-//       .text(date);
+    // // update task in array and re-save to localstorage
+    // tasks[status][index].userInput = userInput;
+    // saveTasks();
   
-//     // replace input with span element
-//     $(this).replaceWith(taskSpan);
+    // // recreate span element with bootstrap classes
+    // var taskSpan = $("<li>")
+    //   .addClass("badge badge-primary badge-pill")
+    //   .text(date);
   
-//     // Pass task's <li> element into auditTask() to check new due date
-//     auditTask($(taskSpan).closest(".listItem"));
-//   })
+    // // replace input with span element
+    // $(this).replaceWith(taskSpan);
+  
+    // // Pass task's <li> element into auditTask() to check new due date
+    // auditTask($(taskSpan).closest(".listItem"));
+  })
 
 
 
